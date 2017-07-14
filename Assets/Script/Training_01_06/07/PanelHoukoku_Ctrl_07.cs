451b774ebb6e61fe3dce63fd008c5d94b4540391
FKNHmd <fukuokaqahamada@gmail.com>
2017-07-14 18:28:48 +0900

    7/14 更新　濱田
    ・Traning_07　70%実装完了
    　残り：
    　　　曲変更の実装
    　　　見た目の修正
    　　　細かいところの実装
    　　　画面遷移の際のフェードインフェードアウト

---
+++ Assets/Script/Training_01_06/07/PanelHoukoku_Ctrl_07.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelHoukoku_Ctrl_07 : MonoBehaviour {
+
+	public GameCtrl_PanelChange GP;
+	public PanelCrossChan_Ctrl PCC;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnEnable () {
+	}
+
+	public void GameGamenhe_Button()
+	{
+		GP.change_panel (GameCtrl_PanelChange.panel.Game);
+	}
+
+	public void play_button()
+	{
+		zannen ();
+	}
+
+	public void teisi_button()
+	{
+		seikai ();
+	}
+
+	public void itiziteisi_button()
+	{
+		zannen ();
+	}
+
+	public void valume_button()
+	{
+		zannen ();
+	}
+
+	public void mute_button()
+	{
+		zannen ();
+	}
+
+	void zannen()
+	{
+		string daimei, section, syousai;
+
+		GP.change_panel (GameCtrl_PanelChange.panel.Crosschan);
+
+		daimei = "バグを見つけられなくて残念ね。。";
+		section = "・次回からはこんな観点で挑戦してね！";
+		syousai = "ちょっと頑張りが足りないみたいね。";
+		PCC.set_crosschan (daimei, section, syousai, PanelCrossChan_Ctrl.crosschan_gazou.Syobon, PanelCrossChan_Ctrl.crosschan_button.Game);
+	}
+
+	void seikai()
+	{
+		string daimei, section, syousai;
+
+		GP.change_panel (GameCtrl_PanelChange.panel.Crosschan);
+		daimei = "クリアおめでとう～♪";
+		section = "・今回の不具合は";
+		syousai = "ボタンの速く連打するとアプリがクラッシュしちゃうバグだよ！";
+		PCC.set_crosschan (daimei, section, syousai, PanelCrossChan_Ctrl.crosschan_gazou.Niko, PanelCrossChan_Ctrl.crosschan_button.Select);
+
+	}
+}
---
+++ Assets/Script/Training_01_06/07/SoundCtrl_07.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCtrl_07 : MonoBehaviour {
+
+	public AudioSource m1;
+	public AudioSource m2;
+	public AudioSource m3;
+	public GameObject Image_Logo;
+
+	int s;
+	AudioSource m;
+	bool saisei_flg;
+
+	// Use this for initialization
+	void Start () {
+		m = m1;
+		saisei_flg = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (saisei_flg == true) {
+			Image_Logo.transform.Rotate (new Vector3(0f,0f,3f));
+		}
+	}
+
+	public void change_sound(int value)
+	{
+		m.Stop ();
+		saisei_flg = false;
+
+		switch (value){
+		case 0:
+			m = m1;
+			break;
+		case 1:
+			m = m2;
+			break;
+		case 2:
+			m = m3;
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void play_button()
+	{
+		m.Play ();
+		saisei_flg = true;
+	}
+
+	public void teisi_button()
+	{
+		m.Stop ();
+		saisei_flg = false;
+	}
+
+	public void itiziteisi_button()
+	{
+		m.Pause ();
+		saisei_flg = false;
+	}
+
+	public void valume_button()
+	{
+	}
+
+	public void mute_button()
+	{
+
+	}
+}
