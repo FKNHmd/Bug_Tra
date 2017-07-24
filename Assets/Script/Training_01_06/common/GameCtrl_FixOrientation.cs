9afdff217ae952ad10380a9ff129a33eff0ca708
FKNHmd <fukuokaqahamada@gmail.com>
2017-07-24 19:00:40 +0900

    7/24 更新　濱田
    ・Traning_01, Traning_04, Traning_06, Traning_07, Traning_08
    　下記のバグを修正
    　「ランドスケープでUIが崩れる」

---
+++ Assets/Script/Training_01_06/common/GameCtrl_FixOrientation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCtrl_FixOrientation : MonoBehaviour {
+
+	ScreenOrientation old_orientation;
+
+	// Use this for initialization
+	void Start () {
+		old_orientation = Screen.orientation;
+
+		Screen.orientation = ScreenOrientation.Portrait;
+
+	}
+
+	void OnDisable ()
+	{
+		Screen.orientation = old_orientation;
+	}
+}
