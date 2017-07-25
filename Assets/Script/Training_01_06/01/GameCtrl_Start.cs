f763604809b652137801d118d7611940e3e4b861
FKNHmd <fukuokaqahamada@gmail.com>
2017-07-25 17:17:32 +0900

    7/25 更新　濱田
    ・Training_01
    　下記のバグを修正
    　「仕様説明で「バグを教えてね♪」の直前の改行が不自然。」

--- Assets/Script/Training_01_06/01/GameCtrl_Start.cs
+++ Assets/Script/Training_01_06/01/GameCtrl_Start.cs
@@ -16,7 +16,7 @@ public class GameCtrl_Start : MonoBehaviour {
 
 		daimei = "ゲームの仕様を説明するね♪";
 		section = "・今回のゲームは";
-		syousai = "キーボードのボタンをTapするとホタルが光るよ！\nキーボードの種類にも注意してね！\n不具合を見つけたら”不具合報告ボタン”をTapして\nバグを教えてね♪";
+		syousai = "キーボードのボタンをTapするとホタルが光るよ！\nキーボードの種類にも注意してね！\n不具合を見つけたら”不具合報告ボタン”をTapしてバグを教えてね♪";
 		PCC.set_crosschan (daimei, section, syousai, PanelCrossChan_Ctrl.crosschan_gazou.Normal, PanelCrossChan_Ctrl.crosschan_button.Game);
 	}
 
