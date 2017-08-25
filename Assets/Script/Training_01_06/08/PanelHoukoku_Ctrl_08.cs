cfaff4984bf75cb308bb3000a14651e613730414
FKNHmd <fukuokaqahamada@gmail.com>
2017-08-25 15:14:49 +0900

    8/25 更新　濱田
    ・Training_08
    　下記の改善
    　「ゲ―ム画面の下の方にクロスちゃんの顔の画像が３つあるが、それぞれ画像の上に左から「★★★★★」、「★★★★」、「★★★」のレア度を入れたい」
    　「ゲーム画面で若干お金が目立たない。文字サイズを変えたり、Boldにしたり、色を変えたりで今より強調させたい」

--- Assets/Script/Training_01_06/08/PanelHoukoku_Ctrl_08.cs
+++ Assets/Script/Training_01_06/08/PanelHoukoku_Ctrl_08.cs
@@ -53,7 +53,7 @@ public class PanelHoukoku_Ctrl_08 : MonoBehaviour {
 		section = "・今回の不具合は";
 		syousai = "ガチャをひいて、結果がわかった後にサスペンド/レジューム(サスレジ)すると、コインが減ってない状態でまたガチャがひけてしまうバグだよ！\n"
 			+ "今回のバグはユーザーが得する不具合だけど、本番のQAではコインだけ減ってキャラを取得できないといったユーザーが損するといったこともないか確認してね！"
-			+ "また、ガチャ結果がわかった瞬間にネットワーク切断やブラウザバック等で問題発生することもあるから注意だよ！";
+			+ "サスレジ以外にネットワーク切断やブラウザバック等で問題発生することもあるから注意だよ！";
 		PCC.set_crosschan (daimei, section, syousai, PanelCrossChan_Ctrl.crosschan_gazou.Niko, PanelCrossChan_Ctrl.crosschan_button.Select);
 
 	}
