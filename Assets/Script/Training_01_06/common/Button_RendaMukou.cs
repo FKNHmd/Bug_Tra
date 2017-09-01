d9f2f245081321be7bd8544c928bd45dc51d89f3
FKNHmd <fukuokaqahamada@gmail.com>
2017-09-01 15:21:37 +0900

    9/1 更新　濱田
    ・全体
    　下記のバグを修正中
    　「ボタン連打系のバグ」

---
+++ Assets/Script/Training_01_06/common/Button_RendaMukou.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_RendaMukou : MonoBehaviour {
+	public Button bt;
+	void OnEnable () {
+		bt.enabled = true;
+	}
+
+	public void NoButtonActivate ()
+	{
+		bt.enabled = false;
+	}
+}
