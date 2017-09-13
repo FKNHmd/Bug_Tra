10897b91bab528830eb39c61b805ab292989f92a
FKNHmd <fukuokaqahamada@gmail.com>
2017-09-13 16:08:02 +0900

    9/13 更新　濱田
    ・チュートリアルのがわを作成
    ・初回起動の時のみTop画面からチュートリアルに遷移する様に実装

---
+++ Assets/Script/Tutorial/TutorialMgr_Flg.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMgr_Flg : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt ("TutorialFlg", 1);
+	}
+}
