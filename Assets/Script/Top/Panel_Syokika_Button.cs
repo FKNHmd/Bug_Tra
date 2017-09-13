612e5fb2fa43777d2e7732b2282227a5d6159f2e
FKNHmd <fukuokaqahamada@gmail.com>
2017-09-13 16:40:53 +0900

    9/13 更新　濱田
    ・Top画面
    　各ステージのクリア状況と初回起動かどうかの情報を初期化する機能を実装

---
+++ Assets/Script/Top/Panel_Syokika_Button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_Syokika_Button : MonoBehaviour {
+	public void syokika () 
+	{
+		PlayerPrefs.DeleteAll ();
+		this.gameObject.SetActive (false);
+	}
+}
