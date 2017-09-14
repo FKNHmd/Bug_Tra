58b9890866fe5bb56c05709c71111491e05e7ddb
AyatakaKaji <fukuokaqaakitaka@gmail.com>
2017-09-14 13:14:06 +0900

    9/14 梶原更新
    Training03に失敗画面、フェードアウトの追加
    GmeCtrl_PanelChange.csを少し編集

--- Assets/Script/Training_03/GameCtrl_CanvasCtrl_03.cs
+++ Assets/Script/Training_03/GameCtrl_CanvasCtrl_03.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class GameCtrl_CanvasCtrl_03 : MonoBehaviour {
 
 	public GameObject CCC;
+    public GameObject gamePanel;
+    public GameCtrl_PanelChange GP;
+    public GameManager_03 gameMar;
 
-	public void CanvasCrossChanONOFF (bool onoff)
+
+    public void CanvasCrossChanONOFF (bool onoff)
 	{
 		CCC.SetActive (onoff);
-	}
+        gamePanel.SetActive(onoff);
+
+    }
+
+    public void Start_Button()
+    {
+        gameMar.GameInitilize();
+        GP.change_panel(GameCtrl_PanelChange.panel.Game);
+    }
 }
