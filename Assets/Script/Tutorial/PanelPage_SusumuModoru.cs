2a664aa2c319b6a204cd6f2749d7344e9c81fb34
FKNHmd <fukuokaqahamada@gmail.com>
2017-09-14 14:43:34 +0900

    9/14 更新　濱田
    ・チュートリアル
    　　１～１０ページ(中身は空)を実装
    　　進む、戻るボタンでページ切り替え可能

---
+++ Assets/Script/Tutorial/PanelPage_SusumuModoru.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelPage_SusumuModoru : MonoBehaviour {
+
+	int page;
+	GameObject[] gobj = new GameObject[10];
+
+	// Use this for initialization
+	void Start () {
+		string str;
+		page = 1;
+
+		for (int i = 0; i < 10; i++) {
+			str = "PanelPage" + (i + 1);
+			gobj[i] = GameObject.Find (str);
+		}
+		page_activate (page);
+	}
+
+	void page_activate (int n)
+	{
+		for (int i = 0; i < 10; i++) {
+			if (i == (n - 1)) {
+				gobj[i].SetActive (true);
+			} else {
+				gobj[i].SetActive (false);
+			}
+		}
+	}
+
+	public void susumu ()
+	{
+		page++;
+		if (page >= 10) {
+			page = 10;
+		}
+		page_activate (page);
+	}
+
+	public void modoru ()
+	{
+		page--;
+		if (page <= 1) {
+			page = 1;
+		}
+		page_activate (page);
+	}
+}
