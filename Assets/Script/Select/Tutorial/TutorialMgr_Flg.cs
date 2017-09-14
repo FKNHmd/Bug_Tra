a8ac86845c355a059b89ecb37afd096ab56ae54d
FKNHmd <fukuokaqahamada@gmail.com>
2017-09-14 16:08:24 +0900

    9/14 更新　濱田
    ・チュートリアル
    　チュートリアルのScene削除
    　セレクト画面にチュートリアルを実装(中身空)

---
+++ Assets/Script/Select/Tutorial/TutorialMgr_Flg.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMgr_Flg : MonoBehaviour {
+
+	GameObject gobj;
+
+	// Use this for initialization
+	void Start () {
+		gobj = GameObject.Find ("Canvas Tutorial");
+		if (PlayerPrefs.GetInt ("TutorialFlg") == 1) {
+			gobj.SetActive (false);
+		} else {
+			PlayerPrefs.SetInt ("TutorialFlg", 1);
+		}
+	}
+
+	public void tutorial_start ()
+	{
+		gobj.SetActive (true);
+	}
+
+	public void tutorial_close ()
+	{
+		gobj.SetActive (false);
+	}
+}
